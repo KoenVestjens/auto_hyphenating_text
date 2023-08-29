5a4d3324f03c632392c4c6c0bed818a5e447fa1f
Koen6095 <koenvestjens@gmail.com>
2023-08-29 11:48:37 +0200

    Updated pubspec

--- lib/auto_hyphenating_text.dart
+++ lib/auto_hyphenating_text.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:hyphenator/hyphenator.dart';
 import 'package:flutter/gestures.dart';
+import 'package:hyphenator_impure/hyphenator.dart';
 
 enum AutoHyphenatingTextStyles {
   bold,
